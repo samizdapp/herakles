f5a8669e59d64002c54f099ee42bbe8061abaf7a
Ryan Bennett <nomad.ry@gmail.com>
2022-05-22 04:09:29 -0600

    remove yggdrasil from this boilerplate

--- service/roamer/update.sh
+++ service/roamer/update.sh
@@ -4,7 +4,7 @@ do
 	echo "Press [CTRL+C] to stop.."
     WAN_ADDR=$(dig +short txt ch whoami.cloudflare @1.0.0.1 | tr -d '"')
     LAN_ADDR=$(upnpc -l | grep "Local LAN ip address" | cut -d: -f2)
-    upnpc -r 80 8889 TCP
     echo "[\"$WAN_ADDR:8889\",\"$LAN_ADDR\"]" > /roamer/addresses
+    upnpc -r 80 8889 TCP
 	sleep 3600
 done
