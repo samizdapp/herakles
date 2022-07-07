8bca403da71855c78fe7a58fb948e9e0400b8c3a
Ryan Bennett <nomad.ry@gmail.com>
2022-07-06 21:23:06 -0600

    WIP

--- local.sh
+++ local.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if ! [ -f "./daemon/next/.next" ]
+then
+pushd ./daemon/next
+npm install
+npm run build
+popd
+fi
+
 docker-compose down
 docker-compose -f docker-compose.yml -f docker-compose.local.yml up --build
 
