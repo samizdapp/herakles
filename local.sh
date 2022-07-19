c5d1e0558b1fdb501c20b6ce4fdcf0d0d5c51c74
Ryan Bennett <nomad.ry@gmail.com>
2022-07-18 20:46:50 -0600

    tmp remove wifi connect

--- local.sh
+++ local.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-if ! [ -f "./daemon/next/.next" ]
-then
-pushd ./daemon/next
-npm install
-npm run build
-popd
-fi
+# if ! [ -f "./daemon/next/.next" ]
+# then
+# pushd ./daemon/next
+# npm install
+# npm run build
+# popd
+# fi
 
-if ! [ -f "./lib/node_modules" ]
-then
-pushd ./lib
-npm install
-popd
-fi
+# if ! [ -f "./lib/node_modules" ]
+# then
+# pushd ./lib
+# npm install
+# popd
+# fi
 
-if ! [ -f "./app/node_modules" ]
-then
-pushd ./app
-npm install
-popd
-fi
+# if ! [ -f "./app/node_modules" ]
+# then
+# pushd ./app
+# npm install
+# popd
+# fi
 
 
 docker-compose down
