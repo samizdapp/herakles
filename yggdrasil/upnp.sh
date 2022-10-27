f8cc2315140e663c88b482c37458f38c679f57c7
Joshua Carter <carter.joshua.603@gmail.com>
2022-10-26 18:44:39 -0700

    Add status calls to yggdrasil_upnp

--- yggdrasil/upnp.sh
+++ yggdrasil/upnp.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+source /usr/bin/status.sh
+
+send_status "yggdrasil_upnp" "WAITING" "Starting."
+
 echo "5000" > /yggdrasil/port
 rm /yggdrasil/peer
 
 if $SKIP_UPNP == "true"; then
     echo "skip upnp"
-    sleep infinity
+    while true; do
+        send_status "yggdrasil_upnp" "WAITING" "SKIP_UPNP set, paused."
+        sleep 60
+    done
 fi
 
 clear_upnp_table(){
@@ -33,6 +40,7 @@ clear_upnp_table(){
     done
     echo "done"
 }
+
 echo "Press [CTRL+C] to stop.."
 PORT=$(cat /yggdrasil/port)
 WAN_ADDR=$(dig +short txt ch whoami.cloudflare @1.0.0.1 | tr -d '"')
@@ -41,21 +49,33 @@ RETRY="true"
 while ! upnpc -r 5000 $PORT TCP
 do
     PORT=$(($PORT + 1))
+
+    send_status "yggdrasil_upnp" "WAITING" "Failed opening: $(($PORT - 1)), attempting to open: $PORT"
+
     echo "increment port $PORT"
     echo "$PORT" > /yggdrasil/port
 
     if [ "$PORT" == "5010" ]; then
         if [ "$RETRY" == "true" ]; then
             RETRY="false"
+
+            send_status "yggdrasil_upnp" "WAITING" "Multiple failed attempts, clearing UPnP table!"
+
             clear_upnp_table
         else 
-            sleep infinity
+            while true; do
+                send_status "yggdrasil_upnp" "OFFLINE" "Failed to open a UPnP port, retries maxed out."
+                sleep 60
+            done
         fi
         echo "5000" > /yggdrasil/port
         PORT="5000"
     fi
 done
 
-
 echo "tcp://$WAN_ADDR:$PORT" > /yggdrasil/peer
-sleep infinity  
+
+while true; do
+    send_status "yggdrasil_upnp" "ONLINE" "Successfully opened UPnP port: $PORT"
+    sleep 120
+done
