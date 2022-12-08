4c28d51fdf2d95326326659602f98719c18ff1db
Ryan Bennett <your@mail.com>
2022-12-07 21:25:40 -0700

    move caddyfile to mustache template

--- caddy/start.sh
+++ caddy/start.sh
@@ -6,7 +6,6 @@ echo "waiting for yggdrasil config"
 sleep 1
 done
 
-
 echo "get public key"
 PUB=$(jq '.PublicKey' /etc/yggdrasil-network/backup.conf | tr -d '"')
 echo $PUB
@@ -18,25 +17,11 @@ echo $PLEROMA
 export CADDY="caddy.$P1.$P2.yg"
 export YGGDRASIL="yggdrasil.$P1.$P2.yg"
 
-CF=/etc/caddy/Caddyfile
+YGGDRASIL_TLD="$P1.$P2.yg"
 
+echo "{\"YGGDRASIL_TLD\" : \"$YGGDRASIL_TLD\", \"MDNS_TLD\" : \"$MDNS_TLD\" }" > frontmatter.yaml
 
-cat Caddyfile.header > $CF
-echo "$PLEROMA {" >> $CF
-echo "  reverse_proxy localhost:8009" >> $CF
-echo "}" >> $CF
-echo "$CADDY {" >> $CF
-echo "  file_server browse {" >> $CF
-echo "    root /data/caddy/" >> $CF
-echo "  }" >> $CF
-echo "}" >> $CF
-echo "$YGGDRASIL {" >> $CF
-echo "  file_server {" >> $CF
-echo "    root /yggdrasil/" >> $CF
-echo "  }" >> $CF
-echo "}" >> $CF
-echo "http://samizdapp.localhost, http://localhost, http://$MDNS_TLD {" >> $CF
-cat Caddyfile.footer >> $CF
+mustache frontmatter.yaml Caddyfile.mustache > /etc/caddy/Caddyfile
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile & jobs
 
